46d86c4db212ca5ea2e30b067ed807eebe5178e2
Oliver-Rozo <oliver.rozankovich@outlook.com>
2026-05-01 09:10:05 -0400

    Delete test folder directory

--- test folder/Untitled-1.js
+++
@@ -1,7 +0,0 @@
-fetch('./test.json')
-  .then(response => {
-    if (!response.ok) throw new Error('Network response was not ok');
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
